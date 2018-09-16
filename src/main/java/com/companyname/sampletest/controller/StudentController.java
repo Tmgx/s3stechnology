385457f6122078c4f42ac9bf35b6ad635f00e7da
tmgx <gosple_of_god@yahoo.com>
2018-09-16 12:08:54 -0500

    added delete and put APi's

--- src/main/java/com/companyname/sampletest/controller/StudentController.java
+++ src/main/java/com/companyname/sampletest/controller/StudentController.java
@@ -1,11 +1,16 @@
 package com.companyname.sampletest.controller;
 
+import java.util.Optional;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,6 +34,39 @@ public StudentEntity getStudentById(@PathVariable(value = "id") Long studentId) 
 	return studentRepository.findById(studentId)
 			.orElseThrow((null));
 }
+
+@DeleteMapping("/student/{id}")
+public void deleteStudent(@PathVariable(value="id") Long studentId) {
+	studentRepository.deleteById(studentId);
+}
+
+//@PutMapping("/student/{id}")
+//public StudentEntity updateStudent(@RequestBody StudentEntity student,@PathVariable(value="id") Long StudentId) {
+//	Optional<StudentEntity> student1= studentRepository.findById(StudentId);
+//	if (student1.isPresent()) {
+//		student.setId(StudentId);
+//		return studentRepository.save(student);
+//	}
+//	return new StudentEntity();
+//}
+
+
+@PutMapping("/student/{id}")
+public ResponseEntity<Object> updateStudent1(@RequestBody StudentEntity student,@PathVariable(value="id") Long StudentId) {
+	Optional<StudentEntity> student1= studentRepository.findById(StudentId);
+	if (!student1.isPresent()) {
+		return ResponseEntity.badRequest().build();
+	}
+	student.setId(StudentId);
+	StudentEntity studentUpdated =studentRepository.save(student);
+	return ResponseEntity.ok().body(studentUpdated);
+}
+
+
+
+
+
+
 
 
 }
